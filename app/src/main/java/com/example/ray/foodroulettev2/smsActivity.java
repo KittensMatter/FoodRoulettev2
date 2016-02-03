b6d548578ea898a0d878dda8c6aba25a658545a9
Ray <rrlee123455@gmail.com>
2016-02-03 13:23:30 -0800

    sync

---
+++ app/src/main/java/com/example/ray/foodroulettev2/smsActivity.java
@@ -0,0 +1,11 @@
+package com.example.ray.foodroulettev2;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ray on 2/2/2016.
+ */
+public class smsActivity extends AppCompatActivity {
+
+
+}
