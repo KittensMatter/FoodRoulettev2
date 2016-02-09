cac4b11aee94abac7489a8cb075a73b3a0733026
rrlee123455 <rrlee123455@gmail.com>
2016-02-08 18:54:16 -0800

    spin wheel + randnumgen

--- app/src/main/java/com/example/ray/foodroulettev2/ResultActivity.java
+++ app/src/main/java/com/example/ray/foodroulettev2/ResultActivity.java
@@ -1,29 +1,133 @@
 package com.example.ray.foodroulettev2;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
 
 public class ResultActivity extends AppCompatActivity {
+    private static Bitmap imageOriginal, imageScaled;
+    private static Matrix matrix;
+
+    private ImageView dialer;
+    private int dialerHeight, dialerWidth;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_result);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.content_main);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // load the image only once
+        if (imageOriginal == null) {
+            imageOriginal = BitmapFactory.decodeResource(getResources(), R.drawable.wheel);
+        }
+
+        // initialize the matrix only once
+        if (matrix == null) {
+            matrix = new Matrix();
+        } else {
+            // not needed, you can also post the matrix immediately to restore the old state
+            matrix.reset();
+        }
+
+
+        dialer = (ImageView) findViewById(R.id.imageView_ring);
+        dialer.setOnTouchListener(new MyOnTouchListener());
+        dialer.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onGlobalLayout() {
+                // method called more than once, but the values only need to be initialized one time
+                if (dialerHeight == 0 || dialerWidth == 0) {
+                    dialerHeight = dialer.getHeight();
+                    dialerWidth = dialer.getWidth();
+
+                    // resize
+                    Matrix resize = new Matrix();
+                    resize.postScale((float)Math.min(dialerWidth, dialerHeight) / (float)imageOriginal.getWidth(), (float)Math.min(dialerWidth, dialerHeight) / (float)imageOriginal.getHeight());
+                    imageScaled = Bitmap.createBitmap(imageOriginal, 0, 0, imageOriginal.getWidth(), imageOriginal.getHeight(), resize, false);
+
+                    // translate to the image view's center
+                    float translateX = dialerWidth / 2 - imageScaled.getWidth() / 2;
+                    float translateY = dialerHeight / 2 - imageScaled.getHeight() / 2;
+                    matrix.postTranslate(translateX, translateY);
+
+                    dialer.setImageBitmap(imageScaled);
+                    dialer.setImageMatrix(matrix);
+                }
             }
         });
+
     }
 
+    private class MyOnTouchListener implements OnTouchListener {
+
+        private double startAngle;
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+
+            switch (event.getAction()) {
+
+                case MotionEvent.ACTION_DOWN:
+                    startAngle = getAngle(event.getX(), event.getY());
+                    break;
+
+                case MotionEvent.ACTION_MOVE:
+                    double currentAngle = getAngle(event.getX(), event.getY());
+                    rotateDialer((float) (startAngle - currentAngle));
+                    startAngle = currentAngle;
+                    break;
+
+                case MotionEvent.ACTION_UP:
+
+                    break;
+            }
+
+            return true;
+        }
+
+    }
+    private double getAngle(double xTouch, double yTouch) {
+        double x = xTouch - (dialerWidth / 2d);
+        double y = dialerHeight - yTouch - (dialerHeight / 2d);
+
+        switch (getQuadrant(x, y)) {
+            case 1:
+                return Math.asin(y / Math.hypot(x, y)) * 180 / Math.PI;
+            case 2:
+                return 180 - Math.asin(y / Math.hypot(x, y)) * 180 / Math.PI;
+            case 3:
+                return 180 + (-1 * Math.asin(y / Math.hypot(x, y)) * 180 / Math.PI);
+            case 4:
+                return 360 + Math.asin(y / Math.hypot(x, y)) * 180 / Math.PI;
+            default:
+                return 0;
+        }
+    }
+
+    /**
+     * @return The selected quadrant.
+     */
+    private static int getQuadrant(double x, double y) {
+        if (x >= 0) {
+            return y >= 0 ? 1 : 4;
+        } else {
+            return y >= 0 ? 2 : 3;
+        }
+    }
+    private void rotateDialer(float degrees) {
+        matrix.postRotate(degrees, dialerWidth / 2, dialerHeight / 2);
+
+        dialer.setImageMatrix(matrix);
+    }
 }
